635b2c092aec528cb04372e8b4d55894a2054a31
kolovos@home <kolobosd98@gmail.com>
2022-04-19 22:10:00 +0300

    how to set server to be visible in home network

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@ min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
+# if we want our server to be visible in the home network then => rails server -b 0.0.0.0 -p 80
 port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
