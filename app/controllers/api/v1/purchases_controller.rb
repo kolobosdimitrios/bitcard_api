c45d3fbc7ff687744e28113fc5b47d0e4c7814f0
kolovos@home <kolobosd98@gmail.com>
2022-05-28 21:49:50 +0300

    create purchases for users

--- app/controllers/api/v1/purchases_controller.rb
+++ app/controllers/api/v1/purchases_controller.rb
@@ -1,13 +1,69 @@
 class Api::V1::PurchasesController < ApplicationController
   def create
+
+    puts(user_id_param)
+    user = User.find_by!(user_id_param)
+
+    begin
+      purchase = Purchase.new(
+        purchase_params_create
+      )
+      purchase.user_id = user.id
+      purchase.save
+      successResponse purchase
+    rescue ActiveRecord::RecordNotUnique
+      errorResponse
+    rescue ActiveRecord::NotNullViolation
+      errorResponse
+    end
+
   end
 
   def index
   end
 
   def show
+    puts user_id_param
+    begin
+      user = User.find_by!(user_id_param)
+      successResponse user.purchases
+    rescue ActiveRecord::RecordNotFound
+      errorResponse
+    end
   end
 
   def destroy
+  end
+
+  def user_id_param
+    params.require(:purchase)
+    .permit(
+      :user_id
+    )
+  end
+
+  def purchase_params_create
+    params.require(:purchase)
+    .permit(
+      :price,
+      :time,
+      :pay_method
+    )
+  end
+
+  def errorResponse(data = [])
+    render json: {
+      "status_code": -1,
+      "description": "Error while saving-creating user",
+      "data": data
+    }
+  end
+
+  def successResponse(data = [])
+    render json: {
+      "status_code": 1,
+      "description": "Success",
+      "data": data
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'purchases/create'
+      post 'purchases/create'
       get 'purchases/index'
       get 'purchases/show'
       get 'purchases/destroy'
