e592ffe889fc4507fb1c2635bb18d55a2447f757
kolovos@home <kolobosd98@gmail.com>
2022-12-17 16:59:34 +0200

    added sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,25 @@
 class SessionsController < ApplicationController
 
+    def new 
+    end
 
+    #create a new session
     def create 
-    
-        @user = User.find_by(username: params[:username])
-        #authnticate user credentials
-        if !!@user && @user.authenticate(params[:password])
-            #set session and redirect on success
-            session[:user_id] = @user.id
-            render json: {
-                status_code: 1,
-                description: "Login Successfull",
-                data: "some_key_here"                
-            }
+        user = User.find_by_user_id(requiere_user_id)
+        if user
+            session[:user_id] = user.id
         else
-            #error message on fail
-            render json: {
-                status_code: -1,
-                description: "Unable to Login",
-                data: ""                
-            }
+            render :new
+        end
+    end
+    
+     
+    #destroy session
+    def destroy
+        session[:user_id] = nil   
+    end
+
+    def requiere_user_id
+        params.require(:user_id)
     end
 end
