40fab75da1b0844074bc0190a01de4a7df601453
kolobosdimitrios <kolobosd98@gmail.com>
2022-12-21 20:41:51 +0200

    confused products with purchases in routes, clear purchasescontroller resources

--- app/controllers/api/v1/purchases_controller.rb
+++ app/controllers/api/v1/purchases_controller.rb
@@ -1,70 +1,16 @@
 class Api::V1::PurchasesController < ApplicationController
   def create
-
-    puts(user_id_param)
-    user = User.find_by!(user_id_param)
-
-    begin
-      purchase = Purchase.new(
-        purchase_params_create
-      )
-      purchase.user_id = user.id
-      purchase.save
-      successResponse purchase
-    rescue ActiveRecord::RecordNotUnique
-      errorResponse
-    rescue ActiveRecord::NotNullViolation
-      errorResponse
-    end
-
   end
 
   def index
   end
 
   def show
-    puts user_id_param
-    begin
-      user = User.find_by!(user_id_param)
-      purchases = Purchase.where(user_id: user.id)
-      successResponse purchases
-    rescue ActiveRecord::RecordNotFound
-      errorResponse
-    end
   end
 
   def destroy
   end
 
-  def user_id_param
-    params.require(:purchase)
-    .permit(
-      :user_id
-    )
-  end
-
-  def purchase_params_create
-    params.require(:purchase)
-    .permit(
-      :price,
-      :time,
-      :pay_method
-    )
-  end
-
-  def errorResponse(data = [])
-    render json: {
-      "status_code": -1,
-      "description": "Error while saving-creating user",
-      "data": data
-    }
-  end
-
-  def successResponse(data = [])
-    render json: {
-      "status_code": 1,
-      "description": "Success",
-      "data": data
-    }
+  def update
   end
 end
