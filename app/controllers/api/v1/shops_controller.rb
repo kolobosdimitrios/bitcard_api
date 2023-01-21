4d110615d1e74ffcb1839d31ef18b5c3e00f818b
kolobosdimitrios <kolobosd98@gmail.com>
2023-01-21 20:18:40 +0200

    implemented shops controller and routes

--- app/controllers/api/v1/shops_controller.rb
+++ app/controllers/api/v1/shops_controller.rb
@@ -1,13 +1,64 @@
 class Api::V1::ShopsController < ApplicationController
+  
   def create
+    @shop = Shop.new(purchase_params)
+    if @shop.save
+      render json: @shop
+    else
+      render json: {
+        "status_code": -1,
+        "description": "shop cannot be created",
+        "data": []
+      }
+    end
   end
 
   def index
+    @shops = Shop.all
+    if(@shops)
+      render json: @shops
+    else
+      render json: {
+        "status_code": -1,
+        "description": "no shops found",
+        "data": []
+      }
+    end
   end
 
   def show
+    @shop = Shop.find(params[:id])
+    if(@shop)
+      render json: @shop
+    else
+      render json: {
+        "status_code": -1,
+        "description": "shop with given id not found",
+        "data": []
+      }
+    end
   end
 
   def destroy
+
+  end
+
+  def update
+    @shop = Shop.find(params[:id])
+    if @shop.update(shop_params)
+      # handle a successful update, such as redirecting to a show page
+      render json: @shop
+    else
+      # handle a failed update, such as rendering the edit page again
+      render json: {
+        "status_code": -1,
+        "description": "cannot update shop",
+        "data": []
+      }
+    end
+  end
+
+  def shop_params
+    params.require(:shop).permit(:shop_name, :location_name, :location_latitude, :location_longitude,:description, :location_address)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,12 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :api do
+    namespace :v1 do
+      resources :shops      
+    end
+  end
+
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htm
 end
