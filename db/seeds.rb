4e87bbeccacecc1fc2f7695f2d7fa2821b4828b7
dimitrisKolovos <kolobosd98@gmail.com>
2023-05-24 15:47:38 +0300

    seeds uncommented

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,41 +28,41 @@ require_relative '../lib/populator_fix.rb'
 #     points: 3000
 # )
 
-# Token.populate 50 do |token|
-#     token.token = Faker::Alphanumeric.alpha(number: 64);
-#     token.user_id = User.all.first.id
-# end
+Token.populate 50 do |token|
+    token.token = Faker::Alphanumeric.alpha(number: 64);
+    token.user_id = User.all.first.id
+end
 
-# Shop.populate 50 do |shop|
-#     shop.shop_name = Faker::Company.name,
-#     shop.location_name = Faker::Address.community,
-#     shop.location_latitude = Faker::Address.latitude,
-#     shop.location_longitude = Faker::Address.longitude,
-#     shop.description = Faker::Lorem.sentence,
-#     shop.location_address = Faker::Address.street_address
-# end
-
-
-# Product.populate 15000 do |product|
-#     product.name = Faker::Appliance.brand,
-#     product.value = Faker::Number.decimal(l_digits: 2, r_digits: 2),
-#     product.description = Faker::Appliance.equipment,
-#     product.code = Faker::Barcode.ean(13) ,
-#     product.barcode = Faker::Alphanumeric.alphanumeric(number: 25),
-#     product.shops_id = Shop.all.sample.id
-# end
+Shop.populate 50 do |shop|
+    shop.shop_name = Faker::Company.name,
+    shop.location_name = Faker::Address.community,
+    shop.location_latitude = Faker::Address.latitude,
+    shop.location_longitude = Faker::Address.longitude,
+    shop.description = Faker::Lorem.sentence,
+    shop.location_address = Faker::Address.street_address
+end
 
 
-# Purchase.populate 30 do |purchase|
-#     purchase.tokens_id = Token.all.sample.id;
-#     purchase.shops_id = Shop.all.sample.id
-# end
+Product.populate 15000 do |product|
+    product.name = Faker::Appliance.brand,
+    product.value = Faker::Number.decimal(l_digits: 2, r_digits: 2),
+    product.description = Faker::Appliance.equipment,
+    product.code = Faker::Barcode.ean(13) ,
+    product.barcode = Faker::Alphanumeric.alphanumeric(number: 25),
+    product.shops_id = Shop.all.sample.id
+end
 
-# PurchaseProduct.populate 1500 do |purchase_product|
-#     purchase = Purchase.all.sample
-#     purchase_product.purchases_id = purchase.id;
-#     purchase_product.products_id = Product.where(shops_id: purchase.shops_id).all.sample
-# end
+
+Purchase.populate 30 do |purchase|
+    purchase.tokens_id = Token.all.sample.id;
+    purchase.shops_id = Shop.all.sample.id
+end
+
+PurchaseProduct.populate 1500 do |purchase_product|
+    purchase = Purchase.all.sample
+    purchase_product.purchases_id = purchase.id;
+    purchase_product.products_id = Product.where(shops_id: purchase.shops_id).all.sample
+end
 
 # FavoriteShop.destroy_all
 
@@ -71,7 +71,6 @@ Shop.all.each do |shop|
     working_hours = "{\"monday\":\"09:00-15:00||17:00-00:00\",\"tuesday\":\"09:00-15:00||17:00-00:00\",\"wednesday\":\"09:00-15:00||17:00-00:00\",\"thursday\":\"09:00-15:00||17:00-00:00\",\"friday\":\"09:00-15:00||17:00-00:00\",\"saturday\":\"09:00-15:00||17:00-00:00\",\"sunday\":\"-\"}"
     contact_info = "{\"phones\":[\"6900000001\",\"6900000002\",\"6900000003\"],\"social_media\":{\"instagram\":\"test_shop_instagram\",\"facebook\":\"test_shop_facebook\",\"twitter\":\"test_shop_twitter\"}}"
     shop.update(working_hours: working_hours, contact_info: contact_info)
-    # puts shop.contact_info.class
 end
 
 
