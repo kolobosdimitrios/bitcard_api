614beb72ba61b640687df4c36b7f68b3af6830ab
dimitrisKolovos <kolobosd98@gmail.com>
2023-06-25 21:04:39 +0300

    try to create purchase products

--- db/seeds.rb
+++ db/seeds.rb
@@ -55,21 +55,7 @@ require_relative '../lib/populator_fix.rb'
 
 # end
 
-Token.all.each do |token|
-    Purchase.create(
-        tokens_id: token.id,
-        shops_id: Shop.all.sample.id
-    )
-end
 
-Purchase.all.each do |purchase|
-    for i in 1..5 do
-        PurchaseProduct.create(
-            purchases_id: purchase.id,
-            products_id: Product.where(shops_id: purchase.shops_id).all.sample
-        )
-    end
-end
 
 # puts("Hello")
 # Purchase.populate 30 do |purchase|
@@ -77,11 +63,11 @@ end
 #     purchase.shops_id = Shop.all.sample.id
 # end
 
-# PurchaseProduct.populate 1500 do |purchase_product|
-#     purchase = Purchase.all.sample
-#     purchase_product.purchases_id = purchase.id;
-#     purchase_product.products_id = Product.where(shops_id: purchase.shops_id).all.sample
-# end
+PurchaseProduct.populate 1500 do |purchase_product|
+    purchase = Purchase.all.sample
+    purchase_product.purchases_id = purchase.id;
+    purchase_product.products_id = Product.where(shops_id: purchase.shops_id).all.sample.id
+end
 
 
 
@@ -90,5 +76,4 @@ end
 #     contact_info = "{\"phones\":[\"6900000001\",\"6900000002\",\"6900000003\"],\"social_media\":{\"instagram\":\"test_shop_instagram\",\"facebook\":\"test_shop_facebook\",\"twitter\":\"test_shop_twitter\"}}"
 #     shop.update(working_hours: working_hours, contact_info: contact_info)
 # end
-
 
